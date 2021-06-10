aaaa1095b679d46e5825377ee03b7945bf2a4bd3
bhoomi-wq <om pandey@ompandey365>
2021-06-10 22:00:00 +0530

    done

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintlog,chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,6 +25,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    constraintlog = new Log(230,180,80,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -35,7 +38,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain1 = new Chain(bird.body,constraintlog.body);
 }
 
 function draw(){
@@ -59,6 +62,8 @@ function draw(){
     log4.display();
     log5.display();
 
+    chain1.display();
     bird.display();
     platform.display();
+    constraintlog.display();
 }
